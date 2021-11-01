55108e518d655c454a9f9e9681edb2341ac9fdab
Charlie Weber <charlestweber@gmail.com>
2021-11-01 13:32:06 -0700

    adds migrations and functioning search endpoint(pitchers)

--- FinanceAPI/Controllers/PitchersController.cs
+++ FinanceAPI/Controllers/PitchersController.cs
@@ -37,7 +37,6 @@ namespace FinanceAPI.Controllers
         using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
         {
           csvReader.Context.RegisterClassMap<PitcherMap>();
-          // csvReader.Configuration.MissingFieldFound = null;
           var pitcherRecords = csvReader.GetRecords<Pitcher>().ToList();
           
           _db.Pitchers.AddRange(pitcherRecords);
@@ -45,6 +44,27 @@ namespace FinanceAPI.Controllers
         }
       }
       return NoContent();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Pitcher>>> GetPitchers(string name, int pitches, int salary)
+    {
+      var query = _db.Pitchers.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(pitcher => pitcher.Name == name);
+      }
+      if (pitches != 0)
+      {
+        query = query.Where(pitcher => pitcher.Pitches == pitches);
+      }
+      if (salary != 0)
+      {
+        query = query.Where(pitcher => pitcher.Salary == salary);
+      }
+
+      return await query.ToListAsync();
     }
   }
 }
