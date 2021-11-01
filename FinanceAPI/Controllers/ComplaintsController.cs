1584c53513c4a47479356b3fbe9d40261730c8ed
toddmac12 <toddmac12@gmail.com>
2021-11-01 17:16:42 -0400

    changed csv file header names. Created model and controller for Complaints

---
+++ FinanceAPI/Controllers/ComplaintsController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinanceAPI.Models;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.IO;
+using System.Globalization;
+
+namespace FinanceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComplaintsController : ControllerBase
+    {
+        private readonly FinanceContext _context;
+
+        public ComplaintsController(FinanceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Complaints
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Complaint>>> GetComplaints()
+        {
+            return await _context.Complaints.ToListAsync();
+        }
+
+    }
